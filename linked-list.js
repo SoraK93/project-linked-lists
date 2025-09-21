cc603a9f8264fbcbb9b69e022b0e022bc6892038
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-09-21 12:40:50 +0530

    MODIFIED SIZE (VARIABLE & MODULE), ADD DOCSTRING TO SOME METHODS

--- linked-list.js
+++ linked-list.js
@@ -1,59 +1,73 @@
 class Node {
-  constructor(data = null, nextNode = null) {
-    if (nextNode !== null && !(nextNode instanceof Node)) {
-      throw new Error(
-        "Only null or Node type data are allowed in nextNode parameter"
-      );
-    }
-
+  constructor(data = null) {
     this.value = data;
-    this.nextNode = nextNode;
+    this.nextNode = null;
   }
 }
 
 class LinkedList {
+  // private variables
   #head;
   #tail;
   #size;
 
-  constructor(value) {
-    this.#head = new Node(value);
+  /** LinkedList initializes with `head`, `tail`, and `size` */
+  constructor() {
+    this.#head = null;
     this.#tail = null;
-    this.#size = 1;
+    this.#size = 0;
   }
 
   /** Add value to the end of the linked list
-   * @value {Node}
+   * @value `{Node}`
    */
   append(value) {
     if (value === undefined || value === null) {
       throw new Error("No value provided");
     }
+
     let node = new Node(value);
-    this.#tail = node;
-    node.nextNode = null;
-    this.addToSize();
+
+    if (this.#size === 0) {
+      this.#head = node;
+      this.#tail = node;
+    } else {
+      this.#tail.nextNode = node;
+      this.#tail = node;
+    }
+
+    this.#addToSize();
   }
 
   /** Add value to the start of the linked list
-   * @value {Node}
+   * @value `{Node}`
    */
   prepend(value) {
     if (value === undefined || value === null) {
       throw new Error("No value provided");
     }
+
     let node = new Node(value);
-    node.nextNode = this.#head;
-    this.#head = node;
-    this.addToSize();
+
+    if (this.#size === 0) {
+      this.#head = node;
+      this.#tail = node;
+    } else {
+      node.nextNode = this.#head;
+      this.#head = node;
+    }
+
+    this.#addToSize();
   }
 
+  /** Increase size by 1 for each new Node `append(value)`/ `prepend(value)` */
+  #addToSize() {
+    this.#size++;
+  }
+
+  /** Get instance variable of the linked list */
   get size() {
     return this.#size;
-  }
-
-  addToSize() {
-    return this.#size++;
   }
 
   get head() {
@@ -66,7 +80,8 @@ class LinkedList {
 
 let data2 = new Node("data2");
 // console.log(data2);
-let dataLL1 = new LinkedList("head");
+let dataLL1 = new LinkedList();
+dataLL1.append("tail");
 dataLL1.append("newtail");
-dataLL1.prepend("newhead");
-console.log(dataLL1.size);
+dataLL1.prepend("head");
+console.log(dataLL1.head);
